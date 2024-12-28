0455c3654d664fae3535dbc093297a3a2203ef80
BugHunter-X <askzenai@gmail.com>
2024-12-28 20:58:06 +0100

    Add bugSolution.dart file

---
+++ bugSolution.dart
@@ -0,0 +1,18 @@
+```dart
+List<int> numbers = [1, 2, 3, 4, 5];
+
+// Correct way to check for an empty list
+if (numbers.isEmpty) {
+  print('List is empty');
+} else {
+  print('List is not empty');
+}
+
+// Another example with an empty list
+List<int> emptyNumbers = [];
+if (emptyNumbers.isEmpty) {
+  print('List is empty');
+} else {
+  print('List is not empty');
+}
+```
